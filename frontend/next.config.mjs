8ed4bc547f272908cb3c6cdf6a382581b6fb736b
DYKIM10 <dykim10iufc@gmail.com>
2025-02-20 13:54:05 +0900

    환경 맞추기1

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
-
-module.exports = {
+const nextConfig = {
     env: {
         NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:8080",
     },
 };
+
+export default nextConfig;
